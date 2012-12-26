886ccb670815d7fc27fbed3a6fcf0b14e8c14cd3
Murugan <murugan.amirthavel@gmail.com>
2012-12-26 19:02:32 +0000

    <Murugan>  Created the model using reverse engineering ef tool

---
+++ src/NorthwindowDemo.DomainModel/Models/Current_Product_List.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace NorthwindowDemo.DomainModel.Models
+{
+    public class Current_Product_List
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+    }
+}
